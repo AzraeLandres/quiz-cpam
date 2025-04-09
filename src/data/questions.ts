ab02af6cfdb46d75ffa01222bf22b953e05b3619
Azrael Landres <azraellandres@gmail.com>
2025-04-10 00:54:11 +0200

    content(quiz): added questions

---
+++ src/data/questions.ts
@@ -0,0 +1,48 @@
+export type Question = {
+  question: string;
+  options: string[];
+  answer: number[];
+};
+const questions: Question[] = [
+  {
+    question: "Que signifie l’acronyme CPAM ?",
+    options: [
+      "Caisse Publique d’Assurance Médicale",
+      "Centre de Protection d’Assurance Maladie",
+      "Caisse Primaire d’Assurance Maladie",
+    ],
+    answer: [2],
+  },
+  {
+    question:
+      "Quel est le nom du site en ligne pour suivre ses remboursements santé ?",
+    options: ["MonEspaceSanté.fr", "Ameli.fr", "MaCarteSanté.fr"],
+    answer: [1],
+  },
+  {
+    question:
+      "Quel document faut-il présenter pour se faire rembourser ses soins ?",
+    options: [
+      "Une carte Vitale",
+      "Une ordonnance",
+      "Une attestation de mutuelle",
+    ],
+    answer: [0],
+  },
+  {
+    question: "Que permet la carte Vitale ?",
+    options: [
+      "Accéder aux urgences sans rendez-vous",
+      "Transmettre automatiquement ses infos à la CPAM",
+      "Bénéficier d’une mutuelle gratuite",
+    ],
+    answer: [1],
+  },
+  {
+    question: "Qui est l'alternant qu'il vous faut ?",
+    options: ["Azraël Landres", "A. Landres", "Azraël L."],
+    answer: [0, 1, 2],
+  },
+];
+
+export default questions;

6d71928143ca4d6e516d3be72650dd41445cf4f4
Azrael Landres <azraellandres@gmail.com>
2025-04-10 11:35:26 +0200

    feat: added social molecule

---
+++ src/components/molecules/SocialIcons.tsx
@@ -0,0 +1,33 @@
+import { FaLinkedinIn, FaGithub, FaDownload } from "react-icons/fa";
+
+const SocialIcons = () => {
+  return (
+    <div className="flex w-[10%] gap-2 justify-evenly items-center">
+      <a
+        href="https://www.linkedin.com/in/azraellandres/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-darkblue text-white p-2  rounded hover:bg-indigo-800 transition"
+      >
+        <FaLinkedinIn className="w-4 h-4" />
+      </a>
+      <a
+        href="https://github.com/AzraeLandres"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-darkblue text-white p-2 rounded hover:bg-indigo-800 transition"
+      >
+        <FaGithub className="w-4 h-4" />
+      </a>
+      <a
+        href="/cv.pdf"
+        download
+        className="bg-darkblue text-white p-2 rounded hover:bg-indigo-800 transition"
+      >
+        <FaDownload className="w-4 h-4" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialIcons;

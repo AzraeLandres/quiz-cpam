a05532b4dae4afccd61cfaacbb8e0227213774d7
Azrael Landres <azraellandres@gmail.com>
2025-04-11 01:34:37 +0200

    ui : fix layout

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -2,7 +2,7 @@ import SocialIcons from "./SocialIcons";
 
 const Header = () => {
   return (
-    <header className="flex w-[80%] items-center justify-between p-4 ">
+    <header className="flex w-1/2 max-w-5xl  items-center justify-between ">
       <img src="./logoAz.svg" alt="logo azl " />
       <SocialIcons />
     </header>

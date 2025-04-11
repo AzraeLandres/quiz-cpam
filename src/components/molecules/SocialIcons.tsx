907652a9620aa0aa7be973c5da8590953a5ea04e
Azrael Landres <azraellandres@gmail.com>
2025-04-11 04:12:33 +0200

    force vite resume copy

--- src/components/molecules/SocialIcons.tsx
+++ src/components/molecules/SocialIcons.tsx
@@ -20,7 +20,7 @@ const SocialIcons = () => {
         <FaGithub className="w-4 h-4" />
       </a>
       <a
-        href="../../../public/cv.pdf"
+        href="/cv.pdf"
         download
         className="bg-darkblue text-white p-2 rounded hover:bg-yellow transition"
       >

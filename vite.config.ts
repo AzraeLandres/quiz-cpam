f7aa9fd1d271b3df0e3f1bba431d51117fafd74e
Azrael Landres <azraellandres@gmail.com>
2025-04-11 04:19:35 +0200

    remove

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/cv.pdf",
-          dest: "",
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   base: "/quiz-cpam/",
   server: {
     watch: {

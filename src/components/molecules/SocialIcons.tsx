13b14ad15934a90a57c6e5b6fb94b698a155d762
Azrael Landres <azraellandres@gmail.com>
2025-04-11 04:26:37 +0200

    test

--- src/components/molecules/SocialIcons.tsx
+++ src/components/molecules/SocialIcons.tsx
@@ -20,7 +20,7 @@ const SocialIcons = () => {
         <FaGithub className="w-4 h-4" />
       </a>
       <a
-        href="../../assets/cv.pdf"
+        href="/quiz-cpam/cv.pdf"
         download
         className="bg-darkblue text-white p-2 rounded hover:bg-yellow transition"
       >
